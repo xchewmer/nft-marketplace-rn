656d66b34ea9176e16acc665041c6594b1d7868a
xchewmer <nt-nirvana91@hotmail.com>
2022-07-26 04:47:41 -0500

    first commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { SubInfo } from "./SubInfo";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsBid,
+  DetailsDesc,
+};
